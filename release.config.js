fc8d6740996f344577627697ccbd4b32f801fa59
bisdas <16491515+bisdas@users.noreply.github.com>
2024-11-07 19:33:31 +0530

    ci: change release config

--- release.config.js
+++ release.config.js
@@ -96,7 +96,7 @@ export default {
             '@semantic-release/git',
             {
                 // eslint-disable-next-line no-template-curly-in-string
-                message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
             },
         ],
     ],
